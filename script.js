92e58cede7d22eac7b0683891c6a38ff1c7629ad
sofhi agustina <sofhiagustina7@gmail.com>
2024-01-09 20:13:57 +0700

    example

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-// toggle ico navbar
+// toggle icon navbar
 
 
 // scrol sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.getAttribute('');
-    });
-
-    if(top >= offset && top < offset + height) {
-        // active navbar links
-        navLinks.forEach(links => {
-            links.classList.remove('active');
-            document.querySelector('header nav a[href*=' + id + ']').classList
-        });
-    }
     // sticky header 
     let header = document.querySelector('header');
 
